d3b43be961d9b840a857ebf1781c390a27315308
renanbabinski <renan10babinski@hotmail.com>
2023-06-08 15:54:27 -0300

    added suport to federator config file

--- src/federator/main.py
+++ src/federator/main.py
@@ -1,5 +1,7 @@
 import logging
 import federator
+import argparse
+from conf import read_config_file
 
 logging.basicConfig(
         format='%(asctime)s [%(levelname)s] %(name)s %(message)s',
@@ -9,6 +11,25 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+# Parse command-line arguments
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Federator Configuration')
+    parser.add_argument('-c', '--config', type=str, help='Path to TOML configuration file')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
     logger.debug("Running main aplication...")
-    federator.run()
+
+    args = parse_arguments()
+
+    if args.config:
+        config = read_config_file(args.config)
+
+        if config is not None:
+            print(config)
+
+    else:
+        print("Error: Please provide a TOML configuration file using the -c or --config argument.")
+
+    # federator.run()
